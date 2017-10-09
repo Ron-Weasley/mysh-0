a0aab46a1d23a1f91a096257b59533b7aff95ae6
H.K.Kang <consistant1y@ajou.ac.kr>
2017-10-09 13:10:46 +0900

    <commit>

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,43 @@
 #include <string.h>
-
+#include <unistd.h>
+#include <stdio.h>
 #include "commands.h"
+#define MAX_PATH 100000
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-  return 0;
+if(chdir(argv[1])){
+return -1;
+} else return 0;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
+char* temp=NULL;
 
-  // TODO: Fill it!
-
-  return 0;
+if((temp=getcwd(NULL, MAX_PATH))==0)
+return -1;
+else{
+printf("%s \n", temp);
+return 0;
 }
+  // TODO: Fill it!}
+
+}
+
 
 int validate_cd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+if(strcmp(argv[0], "cd")==0 && argc==2 ){
+return 1;
+}
+return 0;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
   // TODO: Fill it!
-  return 1;
+ if(strcmp(argv[0], "pwd") ==0 && argc ==1) { return 1;}
+return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,34 @@
 #include "utils.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define Max 1000
+
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+int i =0;
+*argv = (char**)malloc(40);
+
+char temp[Max];
+char * token;
+strcpy(temp, command);
+token = strtok(temp, " \n\t");
+
+while (token != NULL) {
+*(*argv+i)=(char*)malloc(40);
+strcpy(*(*argv+i), token);
+i++;
+token = strtok(NULL, " \n\t");
+}
+if ( i==0) {(*argv)[0]=(char*)malloc(4); *argc=1; }
+else *argc =i;
+
+/*printf("# of token : %d \n", i);
+if(argc != NULL) {*argc=i; printf("success and argc : %d \n", *argc);
+printf("%s, %s \n", (*argv)[0], (*argv)[1]);
+ } */
+
+return;
 }
